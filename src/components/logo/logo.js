979c91908ed424ea407038a6f6955c1843240995
Andrew Ormond <drewthedev9@gmail.com>
2019-03-20 00:54:02 -0400

    adding logo tilt

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Tilt from 'react-tilt';
+import './logo.css'
+import brain from './brain.png';
 
 const Logo = () =>{
     return(
-        <div className='ma4 mt0'></div>
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 250, width: 250 }} >
+                <div className="Tilt-inner"><img alt=" brain" src ={brain}/>></div>
+            </Tilt>
+        </div>
     )
 }
 
