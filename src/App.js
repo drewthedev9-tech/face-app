1a1c3084cd7a8a165a0f256e4b67b13a72a254cd
Andrew Ormond <drewthedev9@gmail.com>
2019-03-28 02:27:42 -0400

    styling box done

--- src/App.js
+++ src/App.js
@@ -37,8 +37,28 @@ constructor() {
   super();
   this.state={
     input: '',
-    imageUrl: ''
+    imageUrl: '',
+    box: {},
   }
+}
+
+calculateFaceLocation = (data) =>{
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    // teh calculaions for the box around the face.
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }
+}
+
+displayFaceBox = (box) =>{
+  console.log(box);
+  this.setState({box: box});
 }
 
 // to pick up what is entered into the input box.
@@ -54,17 +74,9 @@ onButtonSubmit = () => {
   // API key code not needed to change , sampe pic in there as well.
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
-      // something wrong here.
       this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        // do something with response
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
 }
 
   render() {
@@ -83,7 +95,7 @@ onButtonSubmit = () => {
           onButtonSubmit={this.onButtonSubmit}
         />
        
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
