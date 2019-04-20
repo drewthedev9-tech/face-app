3f466bb7ab21e10845c76e4e387efd3fc2660ab7
Andrew Ormond <drewthedev9@gmail.com>
2019-04-19 23:29:40 -0400

    added bcrypt for passwords

--- Server-face-app/server.js
+++ Server-face-app/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 
 const app = express();
@@ -15,7 +16,7 @@ const database = {
             id: 123,
             name: 'john',
             email: 'john@gmail.com',
-            password: 'cookies',
+           
             // used to track scores.
             entries: 0,
             // New Date is part of javascript when date gets excecuted.
@@ -26,13 +27,18 @@ const database = {
             id: 124,
             name: 'sally',
             email: 'sally@gmail.com',
-            password: 'bananas',
+          
             // used to track scores.
             entries: 0,
             // New Date is part of javascript when date gets excecuted.
             joined: new Date()
         }
 
+    ], login:[{
+        id:'987',
+        has: '',
+        email : 'john@gmail.com'
+    }
     ]
 }
 
@@ -46,6 +52,14 @@ app.get('/', (req, res)=>{
 // sign in to handle the sign inswith the database above.
 
 app.post('/signin', (req, res)=>{
+   
+     // Load hash from your password DB.
+    bcrypt.compare("apples", '$2a$10$fL5xoeLtGxkLirnWmn.bbuGIgLnRvTfsE857LdeRcyLOfT1PIZd6.', function(err, res) {
+        console.log('first guess', res)
+    });
+    bcrypt.compare("veggies", '$2a$10$fL5xoeLtGxkLirnWmn.bbuGIgLnRvTfsE857LdeRcyLOfT1PIZd6.', function(err, res) {
+        console.log('second guess', res)
+    });
     if(req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password){
             res.json('success');
@@ -111,6 +125,10 @@ app.post('/image',(req, res)=>{
             res.status(400).json('not found');
         }
 })
+
+
+
+
 
 
 app.listen (3000, ()=> {
