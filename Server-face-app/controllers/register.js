32f3f70f4d27d0e59d1c46decd8df0fbaed4fb3c
Andrew Ormond <drewthedev9@gmail.com>
2019-05-07 13:00:18 -0400

    added controller file

---
+++ Server-face-app/controllers/register.js
@@ -0,0 +1,43 @@
+const handleRegister = (req, res,db, bcrypt)=>{
+    // using destructuring can get these thing from re.body(fron the front-end)
+    const {email, name, password} = req.body;
+    const hash = bcrypt.hashSync(password);
+    // knex inserting from the database (users table).
+    // transaction: trx
+    // updates the logon table
+    db.transaction(trx =>{
+        trx.insert({
+            hash:hash,
+            email: email,
+        })
+        .into('login')
+        .returning('email')
+        // then updates the users table.
+        .then(loginemail =>{
+            return trx('users')
+            .returning('*')
+            .insert({
+                // return the array
+                email: loginemail[0],
+                name: name,
+                joined: new Date()
+            })
+            .then(user =>{
+            // responbding with grabbing the first in the database user(who should be the NEW user) of the array.
+            res.json(user[0]);
+        })
+        })
+        // KNEX js
+        // if all the cod above passed, then will commit and run through
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+      
+    // error when people are loggin in if name is the same .
+    .catch(err=> res.status(400).json('unable to register'))
+}
+
+module.exports ={
+handleRegister: handleRegister
+
+};
--- Server-face-app/server.js
+++ Server-face-app/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require ('knex');
+const register = require('./controllers/register')
 
 // connecting to database:
 const db = knex({
@@ -62,44 +63,7 @@ app.post('/signin', (req, res)=>{
 
 // register --> POST = user.
 // registering to a new user so adding to the database.
-app.post('/register', (req, res)=>{
-    // using destructuring can get these thing from re.body(fron the front-end)
-    const {email, name, password} = req.body;
-    const hash = bcrypt.hashSync(password);
-    // knex inserting from the database (users table).
-    // transaction: trx
-    // updates the logon table
-    db.transaction(trx =>{
-        trx.insert({
-            hash:hash,
-            email: email,
-        })
-        .into('login')
-        .returning('email')
-        // then updates the users table.
-        .then(loginemail =>{
-            return trx('users')
-            .returning('*')
-            .insert({
-                // return the array
-                email: loginemail[0],
-                name: name,
-                joined: new Date()
-            })
-            .then(user =>{
-            // responbding with grabbing the first in the database user(who should be the NEW user) of the array.
-            res.json(user[0]);
-        })
-        })
-        // KNEX js
-        // if all the cod above passed, then will commit and run through
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-      
-    // error when people are loggin in if name is the same .
-    .catch(err=> res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res)=>{register.handleRegister(req, res,db, bcrypt)})
 
 // matching id endpoint to get user.
 app.get('/profile/:id', (req, res)=>{
